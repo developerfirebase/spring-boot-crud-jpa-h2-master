dad6f1982315d07a3cb363579cfa2d176f0a348d
aditya <developerfirebase18@gmail.com>
2022-07-14 17:59:06 +0530

    Yes My Initial commit

---
+++ src/test/java/com/tbp/crud/SpringBootCrudJpaH2ApplicationTests.java
@@ -0,0 +1,11 @@
+package com.tbp.crud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootCrudJpaH2ApplicationTests {
+
+
+
+}
